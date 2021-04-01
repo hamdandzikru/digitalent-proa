83e19740b1d23830d236abf39c62e729332e56cd
hamdandzikru <hamdanrobbi@gmail.com>
2021-04-01 14:29:46 +0700

    stage 2.2

--- script2.js
+++ script2.js
@@ -1,15 +1,14 @@
 const printKen = () => {
-  console.log("Ninja Ken");
-};
-
-const printMaster = () => {
-  console.log("Guru Domba");
-};
-
-const call = (callback) => {
-  console.log("Memanggil function callback.");
-  callback();
-};
-
-// Ketik ulang argument ini sebagai printMaster dan konfirmasikan output-nya
-call(printMaster);
+    console.log("Ninja Ken");
+  };
+  
+  // Tambahkan parameter callback ke function call
+  const call = (callback) => {
+    console.log("Memanggil function callback.");
+    // Panggil callback function
+    callback();
+  };
+  
+  // Teruskan printKen sebagai argument dan jalankan function call
+  call(printKen);
+  
