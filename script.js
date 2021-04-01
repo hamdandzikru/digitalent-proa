557c07d1415dbebd00811f5d1f8c6fb1d97fa310
hamdandzikru <hamdanrobbi@gmail.com>
2021-04-01 14:27:39 +0700

    lv 4

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const numbers = [1, 3, 5, 7, 9];
+const numbers = [1, 2, 3, 4];
 
-// Temukan kelipatan 3 dari array numbers dengan menggunakan method find
-// dan tetapkan hasilnya ke constant foundNumber 
-const foundNumber = numbers.find((number) => { return number % 3 === 0 });
+// Dapatkan semua nomor genap dari array numbers dengan menggunakan method filter,
+// dan tetapkan hasilnya ke constant evenNumbers 
 
-// Print foundNumber
-console.log(foundNumber);
+const evenNumbers = numbers.filter((number) => { return number % 2 === 0 });
+
+// Print nilai evenNumbers
+console.log(evenNumbers);
 
 
 const characters = [
-  {id: 1, name: "Ninja Ken", age: 6},
-  {id: 2, name: "Baby Ben", age: 2},
-  {id: 3, name: "Guru Domba", age: 100},
-  {id: 4, name: "Birdie", age: 21}
+  {id: 1, name:"Ninja Ken", age: 14},
+  {id: 2, name:"Baby Ben", age: 5},
+  {id: 3, name:"Guru Domba", age: 100}
 ];
 
-// Temukan object dengan id = 3 dari constant characters,
-// dan tetapkan nilainya ke constant foundCharacter
-const foundCharacter = characters.find((character) => { return character.id === 3 });
+// Dapatkan characters berumur lebih muda dari 20,
+// dan tetapkan hasilnya ke constant underTwenty 
 
-// Print foundCharacter
-console.log(foundCharacter);
+const underTwenty = characters.filter((character) => { return character.age < 20 });
+
+// Print nilai underTwenty
+console.log(underTwenty);
